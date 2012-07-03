1fdba20b8d3ca9a31577ecac3d31cd800aeb1aa0
Dinesh Copoosamy <dinesh@dinesh.co.za>
2012-07-03 23:14:29 +0200

    Updated user service

--- build/angular-drupal.js
+++ build/angular-drupal.js
@@ -104,51 +104,44 @@ angular.module('drupal.services').factory('userService',['$rootScope','$http','d
     var user = {};
     var session_id = null;
 
+    setUser = function(u) {
+      user = u;
+      $rootScope.$broadcast('drupal:UserUpdated');
+    }
+
+    setSessionId = function(sessid) {
+      session_id = sessid;
+    }
+
     return {
         getUser : function () {
             return user;
         },
         login : function(username,password) {
-            $http({method: 'POST', url: drupalConfig.settings.service_endpoint + 'user/login.json', data: {username: username, password: password}}).
-                success(function(data, status, headers, config) {
-                    user = data;
-                    $rootScope.$broadcast('event:loginConfirmed');
-                }).
-                error(function(data, status, headers, config){
-                    switch(status){
-                        case 406:
-                            //noinspection JSUnresolvedFunction
-                            ping();
-                            break;
-                        case 401:
-                        default:
-                            user = { uid: 0 };
-                            $rootScope.$broadcast('event:loginRequired');
-                            break;
-                    }
-                });
+            return $http({method: 'POST', url: drupalConfig.settings.service_endpoint + 'user/login.json', data: {username: username, password: password}}).
+              success(function(data, status, headers, config) {
+                setUser(data.user);
+              });
         },
         logout : function() {
             $http({method: 'POST', url: drupalConfig.settings.service_endpoint + 'user/logout.json', data: {}}).
                 success(function(data, status, headers, config) {
-                    user = data;
-                    $rootScope.$broadcast('event:logoutConfirmed');
+                    setUser(data);
                 });
         },
         ping : function() {
             systemService.connect().then(function(response) {
-                user = response.data.user;
-                session_id = response.data.sessid;
-                $rootScope.$broadcast('drupal:UserUpdated');
+                setUser(response.data.user);
+                setSessionId(response.data.sessid);
             });
         },
         authenticated : function() {
-            if(user.uid > 0) {
-                return true;
-            }
-            else {
-                return false;
-            }
+          if(user.uid > 0){
+            return true;
+          }
+          else {
+            return false;
+          }
         }
     }
 }]);
